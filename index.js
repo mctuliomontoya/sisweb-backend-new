1717e6e81c81d5b23d88a9785c90ab026076b729
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-01 00:55:49 -0700

    feat: add sequelize sync to index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const db = require("./src/models");
 
 import apiRouter from "./src/routes";
 
 const app = express();
 const port = 3000;
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
 app.use(morgan("dev"));
 app.use(express.json());
